eca11bc62489a2fb6f004f945cab0f9c762993b9
John Charlton <coolmule0@hotmail.co.uk>
2025-01-06 15:40:46 +0000

    hough circle

---
+++ src/speckle/circles_detect.py
@@ -0,0 +1,54 @@
+import sys
+import cv2 as cv
+import numpy as np
+ 
+ 
+def main(argv):
+	
+	default_file = './img.png'
+	filename = argv[0] if len(argv) > 0 else default_file
+ 
+	# Loads an image
+	src = cv.imread(cv.samples.findFile(filename), cv.IMREAD_COLOR)
+ 
+	# Check if image is loaded fine
+	if src is None:
+		print ('Error opening image!')
+		print ('Usage: hough_circle.py [image_name -- default ' + default_file + '] \n')
+		return -1
+	
+ 
+	
+	gray = cv.cvtColor(src, cv.COLOR_BGR2GRAY) 
+	
+	rows = gray.shape[0]
+	circles = cv.HoughCircles(gray, cv.HOUGH_GRADIENT, 1, rows / 20,
+							   param1=100, param2=15,
+							   minRadius=0, maxRadius=30)
+	
+ 
+	if circles is not None:
+		circles = np.uint16(np.around(circles))
+		for i in circles[0, :]:
+			center = (i[0], i[1])
+			# circle center
+			cv.circle(src, center, 1, (0, 100, 100), 3)
+			# circle outline
+			radius = i[2]
+			cv.circle(src, center, radius, (255, 0, 255), 3)
+	
+	# average circle radius
+	radii = circles[:, :, 2]
+	average_radius = np.average(radii)
+
+
+	print("average radius: ", average_radius)
+	cv.imshow("detected circles", src)
+	cv.waitKey(0)
+	
+ 
+	return 0
+ 
+ 
+if __name__ == "__main__":
+	main(sys.argv[2:])
